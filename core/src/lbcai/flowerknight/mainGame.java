4acdd2c5da3bb6288af60a66f083c03ffa6d2265
lbcai <73155687+lbcai@users.noreply.github.com>
2021-01-28 23:40:19 -0800

    Player can now move the sprite.

--- core/src/lbcai/flowerknight/mainGame.java
+++ core/src/lbcai/flowerknight/mainGame.java
@@ -1,20 +1,35 @@
 package lbcai.flowerknight;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class mainGame extends ApplicationAdapter {
-	SpriteBatch player;
+	// Object that "contains" drawn sprites; draw stuff to it to see the stuff. Dispose or else memory will leak.
+	SpriteBatch batch;
+	
+	// Underlying OpenGL texture (image). Dispose or else memory will leak.
 	Texture playerTexture;
+	
+	// Sprite object that has the coordinates and is hooked up to the texture.
+	Sprite playerSprite;
+	
 	
 	
 	@Override
 	public void create () {
-		player = new SpriteBatch();
+		int winHeight = Gdx.graphics.getHeight();
+		int winWidth = Gdx.graphics.getWidth();
+		
+		batch = new SpriteBatch();
 		playerTexture = new Texture("badlogic.jpg");
+		playerSprite = new Sprite(playerTexture);
+		
+		playerSprite.setPosition(winWidth/2 - playerSprite.getWidth()/2, 0);
 	}
 
 	@Override
@@ -29,16 +44,25 @@ public class mainGame extends ApplicationAdapter {
 	
 	@Override
 	public void render () {
+		
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			playerSprite.translateX(10.0f);
+		} else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			playerSprite.translateX(-10.0f);
+		}
+		
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		player.begin();
-		player.draw(playerTexture, 0, 0);
-		player.end();
+		batch.begin();
+		playerSprite.draw(batch);
+		batch.end();
+		
+		
 	}
 	
 	@Override
 	public void dispose () {
-		player.dispose();
+		batch.dispose();
 		playerTexture.dispose();
 	}
 }
